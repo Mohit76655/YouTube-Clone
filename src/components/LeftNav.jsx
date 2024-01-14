4029eb8d79d317ac04bf7da1f1f6a0cae903bf9d
Mohit76655 <mohit76655@gmail.com>
2024-01-14 21:07:19 +0530

    Left-NavBar-Complete

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { categories } from "../utils/constants";
+import { Context } from "../context/contextApi";
 
 const LeftNav = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { selectCategorie, setSelectCategorie, mobileMenu } =
+    useContext(Context);
 
-export default LeftNav
+  const navigate = useNavigate();
+
+  const clickHandler = (name, type) => {
+    switch (type) {
+      case "category":
+        return setSelectCategorie(name);
+      case "home":
+        return setSelectCategorie(name);
+      case "menu":
+        return false;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className=" md:block w-[240px] overflow-y-auto h-full py-4 bg-[#0F0F0F] absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all scrollbar ">
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment>
+              <LeftNavMenuItem
+                text={item.type === "home" ? "Home" : item.name}
+                icon={item.icon}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
+                className={`${
+                  selectCategorie === item.name ? "bg-white/[0.15]" : ""
+                }`}
+              />
+              {item.divider && <hr className="my-5 border-white/[0.2]" />}
+            </React.Fragment>
+          );
+        })}
+        <hr className="my-5 border-white/[0.2]" />
+        <div className="text-white/[0.5] text-[12px]">Clone By Mohit</div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftNav;
