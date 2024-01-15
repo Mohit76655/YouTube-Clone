d152ef46a8e27b47ef50d1c2b2c7eefb5711c589
Mohit76655 <mohit76655@gmail.com>
2024-01-15 22:43:37 +0530

    Feed start

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,6 +6,9 @@ const Feed = () => {
   return (
     <div className="flex flex-row h-[calc(100%-56px)] ">
       <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"></div>
+      </div>
     </div>
   );
 };
